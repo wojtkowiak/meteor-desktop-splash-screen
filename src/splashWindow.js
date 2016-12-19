7901c3b0a3dbfd63cddadd90f345ae561dd98f85
tzapu <alex@tzapu.com>
2016-12-19 19:14:31 +0200

    Update splashWindow.js

--- src/splashWindow.js
+++ src/splashWindow.js
@@ -21,7 +21,6 @@ export default class SplashWindow {
             frame: false,
             transparent: true,
             resizable: false,
-            //center: true, //commenting this centers the window on macOS
             show: false,
             webPreferences: { nodeIntegration: false }
         };
